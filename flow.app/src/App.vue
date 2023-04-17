6e5c8a36bd3f879a1279810d2a9723250781f350
alimaria <alimaria@umich.edu>
2023-04-17 12:28:57 -0400

    base site
    first push of flow website

---
+++ flow.app/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="app">
+		<!-- Sidebar -->
+		<Sidebar />
+
+		<!-- Content -->
+		<router-view />
+	</div>
+</template>
+
+<script setup>
+import Sidebar from './components/Sidebar.vue'
+</script>
+
+<style lang="scss">
+:root {
+	--primary: #740000;
+	--primary-alt: #740000;
+	--grey: #000000;
+	--dark: #fe6ca7;
+	--dark-alt: #e34984;
+	--light: #ded8c8;
+	--sidebar-width: 300px;
+}
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Fira sans', sans-serif;
+}
+
+body {
+	background: var(--light);
+}
+
+button {
+	cursor: pointer;
+	appearance: none;
+	border: none;
+	outline: none;
+	background: none;
+}
+
+.app {
+	display: flex;
+
+	main {
+		flex: 1 1 0;
+		padding: 2rem;
+
+		@media (max-width: 1024px) {
+			padding-left: 6rem;
+		}
+	}
+}
+</style>
---
+++ flow.app/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+	<main id="Home-page">
+		<div class="logo">
+			<img :src="logoURL" alt="logo" /> 
+		</div>
+		<p>This is the home page</p>
+	</main>
+</template>
+
+<script setup>
+import logoURL from '/flowgo.png'
+</script>
+
+<style lang="scss" scoped>
+.logo {
+		margin: 0 -0.6rem;
+		margin-bottom: 1rem;
+
+		img {
+			width: 14rem;
+		}
+	}
+</style>
